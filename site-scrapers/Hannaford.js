73868c8222aa3b16eb33ece8f88ca389911f4cd5
Zach Peyton <zachary.peyton@gmail.com>
2021-02-23 18:04:12 -0500

    Hannaford: some progress on getting facilityId

--- site-scrapers/Hannaford.js
+++ site-scrapers/Hannaford.js
@@ -1,5 +1,6 @@
 const s3 = require("../lib/s3");
 const sites = require("../data/sites.json");
+const https = require("https");
 
 const siteName = "Hannaford";
 const site = sites[siteName];
@@ -27,42 +28,83 @@ module.exports = async function GetAvailableAppointments(browser) {
 
 async function ScrapeWebsiteData(browser) {
     const page = await browser.newPage();
-    await page.goto(site.website);
-    await page.solveRecaptchas().then(({ solved }) => {
-        if (solved.length) {
-            return page.waitForNavigation();
-        } else {
-            return;
+    const results = [];
+    for (let [i, loc] of site.locations.entries()) {
+        await page.goto(site.website);
+        await page.solveRecaptchas().then(({ solved }) => {
+            if (solved.length) {
+                return page.waitForNavigation();
+            } else {
+                return;
+            }
+        });
+
+        //if (!results[i]) {
+        await page.evaluate(
+            () => (document.getElementById("zip-input").value = "")
+        );
+        await page.type("#zip-input", loc.zip);
+        const [searchResponse, ...rest] = await Promise.all([
+            Promise.race([
+                page.waitForResponse(
+                    "https://hannafordsched.rxtouch.com/rbssched/program/covid19/Patient/CheckZipCode"
+                ),
+                page.waitForNavigation(),
+            ]),
+            page.click("#btnGo"),
+        ]);
+
+        let result = "";
+        try {
+            result = (await searchResponse.buffer()).toString();
+            //console.log("got data back " + result);
+        } catch (e) {
+            //console.log("buffer failed, probably because the page changed during the 'waitForResponse'");
         }
-    });
 
-    const results = {};
+        //await page.setRequestInterception(true);
 
-    for (const loc of [...new Set(site.locations)]) {
-        if (!results[loc.zip]) {
-            await page.evaluate(
-                () => (document.getElementById("zip-input").value = "")
-            );
-            await page.type("#zip-input", loc.zip);
-            const [searchResponse, ...rest] = await Promise.all([
-                Promise.race([
-                    page.waitForResponse(
-                        "https://hannafordsched.rxtouch.com/rbssched/program/covid19/Patient/CheckZipCode"
-                    ),
-                    page.waitForNavigation(),
-                ]),
-                page.click("#btnGo"),
-            ]);
-            const result = (await searchResponse.buffer()).toString();
-            //if there's something available, log it with a unique name so we can check it out l8r g8r
-            if (result.indexOf(noAppointmentMatchString) == -1) {
-                // theoretically found appointments
-                if (!process.env.DEVELOPMENT) {
-                    await s3.savePageContent(`${siteName}-${loc.zip}`, page);
+        //if there's something available, log it with a unique name so we can check it out l8r g8r
+        if (result.indexOf(noAppointmentMatchString) == -1) {
+            console.log("didn't find no appointments string");
+
+            const pageUrl = await page.url();
+            if (!process.env.DEVELOPMENT) {
+                await s3.savePageContent(`${siteName}-${loc.zip}`, page);
+            }
+
+            if (pageUrl.includes("Schedule")) {
+                //
+                const url =
+                    "https://hannafordsched.rxtouch.com/rbssched/program/covid19/Calendar/PatientCalendar";
+
+                const postData = ``;
+                const cookies = await page.cookies();
+
+                await page.waitForSelector("#address:not(:empty)");
+                // the same facility could be returned for multiple zips
+                // if the same facility is returned we don't need to get the same schedule data again
+
+                //const handle = await page.evaluateHandle(() => ({window, document}));
+                let facilityId = await page.evaluate(
+                    () => window.$.calendar.facilityId
+                );
+                let address = await page.evaluate(() =>
+                    window.$("#address").html().trim()
+                );
+
+                console.log(address.trim());
+                if (address == loc.street) {
+                    result = " appointments found ";
+                } else {
+                    result = noAppointmentMatchString;
                 }
             }
-            results[loc.zip] = result;
         }
+
+        results[i] = result;
+
+        //} // end if results
     }
 
     return results;
